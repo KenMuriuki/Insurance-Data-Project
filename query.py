1b22e9379dad27e008e399cbd900cf70ef94e070
KenMuriuki <muriukikenedy@gmail.com>
2023-12-04 18:01:16 +0300

    deploying app maybe?

--- query.py
+++ query.py
@@ -1,23 +1,37 @@
-import mysql.connector
+# import mysql.connector
+# import streamlit as st
+
+# # connection
+# conn=mysql.connector.connect(
+#     host="localhost",
+#     port="3306",
+#     user="root",
+#     passwd="",
+#     db="insurancedb"
+# )
+
+# c=conn.cursor()
+
+# # fetch data
+# def view_all_data():
+#     c.execute("select * from insurance order by id asc")
+#     data = c.fetchall()
+#     return data
+
 import streamlit as st
+import numpy as np
+import pandas as pd
+import pymysql
+#import pyodbc
+from sqlalchemy import create_engine, text
 
-# connection
-conn=mysql.connector.connect(
-    host="localhost",
-    port="3306",
-    user="root",
-    passwd="",
-    db="insurancedb"
-)
+# connecting to the database
+engine = create_engine("mysql+pymysql://root:@localhost/insurancedb")
 
-c=conn.cursor()
-
-# fetch data
+@st.cache_data
 def view_all_data():
-    c.execute("select * from insurance order by id asc")
-    data = c.fetchall()
-    return data
-
+    df = pd.read_sql("SELECT * FROM insurance", engine.connect())
+    return df
 
 # Initialize connection.
 # conn = st.connection('mysql', type='sql')
